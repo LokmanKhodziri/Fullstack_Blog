b52b60b2d2a1d5055102f185d94364bcf183f504
lokmankhodziri <lokmankhodziri@gmail.com>
2024-05-28 22:43:05 +0800

    login and logout function with crudentials

--- api/index.js
+++ api/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = 5050;
 
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdjf134as';
+
 app.use(cors({
     origin: 'http://localhost:3000',
-    methods: ['POST'],
+    methods: ['POST', 'GET'],
     allowedHeaders: ['Content-Type'],
+    credentials: true,
+    origin: 'http://localhost:3000',
 }));
-
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://admin:admin1234@cluster0.nqggchq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('Connected to MongoDB'))
@@ -23,7 +32,10 @@ app.post('/register', async (req, res) => {
         return res.status(400).json({ message: 'Username and password are required' });
     }
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt)
+        });
         res.status(200).json(userDoc);
     } catch (e) {
         console.error(e);
@@ -31,6 +43,31 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({ username, id: userDoc._id, }, secret, {}, (error, token) => {
+            if (error) throw error;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('Wrong username or password');
+    }
 });
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+})
+
+app.listen(port);
--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,12 +1,47 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5050/profile', {
+            credentials: 'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            })
+        });
+    }, []);
+
+    function logout() {
+        fetch('http://localhost:5050/logout', {
+            method: 'POST',
+            credentials: 'include',
+        }).then(response => {
+            if (response.ok) {
+                setUsername(null);
+            }
+        });
+        setUsername(null);
+    }
+
     return (
         <header>
             <Link to="/" className="logo">MyBlog</Link>
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {username && (
+                    <>
+                        <Link to="/create">Create New Post</Link>
+                        <a onClick={logout}>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
             </nav>
         </header>
     );
